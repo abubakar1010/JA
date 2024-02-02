9651245a6e21220759ba68d3944f25f5116f9eb7
Abubakar1010 <abubakar850772@gmail.com>
2024-02-02 15:11:31 +0600

    basic of arrow function

--- Fundamental/15_arrowFunction.js
+++ Fundamental/15_arrowFunction.js
@@ -1,8 +1,32 @@
 
+const user = {
+    name: "Osman",
 
-function greeting(parameterVariable) {
-    // This line prints 'Hello, World!' to the console:
+    price: 1111,
 
-    console.log('Hello, World!');
-console.log(parameterVariable)
+    massage: function(){
+        console.log(`${this.name}, welcome to our community `);
+    }
 }
+
+// user.massage()
+
+user.name = "Omar"
+
+// user.massage()
+
+// note: basic arrow function
+
+let arrowFunction = (num1, num2) => {
+
+    return num1 + num2
+}
+
+// console.log(arrowFunction( 8, 4))
+
+// note: An implicit return is a way of returning a value from a function without explicitly using the return keyword. 
+
+let implicitReturnFunction = (input1, input2) => ( input1 + input2)
+
+// console.log(implicitReturnFunction(8, 4));
+
