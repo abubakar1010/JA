58cecf531b245a0baa8ee0bd84e3d52010e0dc1d
Abubakar1010 <abubakar850772@gmail.com>
2024-02-07 17:43:37 +0600

    more about this and arrow function with implicit and explicit return

--- Fundamental/15_arrowFunction.js
+++ Fundamental/15_arrowFunction.js
@@ -30,3 +30,66 @@ let implicitReturnFunction = (input1, input2) => ( input1 + input2)
 
 // console.log(implicitReturnFunction(8, 4));
 
+
+
+const client = {
+    clientName: "Osman",
+
+    profession: "Senior Software Engineer",
+
+    welcomeMassage: function() {
+
+        console.log(`${this.clientName}, Welcome to our team`);
+
+        console.log(this);
+    }
+}
+
+// client.welcomeMassage()
+
+// client.clientName = "Omar"
+// client.welcomeMassage()
+
+// console.log(this);
+
+// function coffee(){
+//     console.log(this);
+// }
+
+// coffee()
+
+
+// note: syntax of arrow function
+
+const coffee = () => {
+    console.log("this is good or bad");
+
+    let userName = "Abu Bakar"
+
+    console.log(this.userName);
+} 
+
+// coffee()
+
+//basic syntax
+
+// const add = (num11 , num12) => {
+//     return num11 * num12
+// }
+
+// note: implicitReturnFunction 
+
+const add = (num11 , num12) => num11 * num12
+
+//note: another syntax of implicitReturnFunction 
+
+const addtwo = (val1, val2) => (val1 + val2)
+
+// note: if use curly braces then must need to use return keyword but don't need to use if use parentheses. 
+
+// i-q: so tow syntax number1: const variableName = () = {this function need to "return" keyword} number2: const variableName = () = (this function don't need to "return" keyword)
+
+// console.log(add(5, 10));
+
+const implicitObj = () => ({userName: "abu bakar"})
+
