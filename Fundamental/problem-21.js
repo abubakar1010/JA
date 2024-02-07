f63d03d7b0c6e8ad8f59d22b54c4717f32915f7c
Abubakar1010 <abubakar850772@gmail.com>
2024-02-07 18:21:42 +0600

    added multiplication table

--- Fundamental/problem-21.js
+++ Fundamental/problem-21.js
@@ -16,3 +16,18 @@ function findLowestNumber(values){
 }
 
 console.log(findLowestNumber([167, 190, 120, 165, 137]));
+
+//create multiplication table
+
+for ( i = 1; i <= 10; i++){
+    
+    console.log(` this is multiplication table of ${i}`);
+    
+    for ( a = 1; a <= 10; a++){
+        
+        
+        console.log( ` ${a} * ${i} = ` , a * i);
+    }
+
+
+}
