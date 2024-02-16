3805b5ec9e71809c367ed318d281b1fcd6f0da88
Abubakar1010 <abubakar850772@gmail.com>
2024-02-16 21:33:51 +0600

    more about map method of array

--- Fundamental/17_maps.js
+++ Fundamental/17_maps.js
@@ -14,5 +14,24 @@ console.log(map);
 //note: maps knew for his uniqueness, maps not allows duplicate values. In js type of map is object
 
 for ( let [key, value] of map){
-    console.log(key, " :" , value);
+    // console.log(key, " :" , value);
 } 
+
+// note: maps hold data in key value pair but key should be any data types.And map also remember the sequence
+
+
+//note: map syntax  for add element ---> name.set("key", "value")
+
+const fruits = new Map()
+
+fruits.set("name", "mango")
+fruits.set("price", 200)
+
+console.log(fruits);
+
+console.log(fruits.get("name"))
+
+// note: .get method use for get value
+
+console.log(fruits.delete("name"))
+
