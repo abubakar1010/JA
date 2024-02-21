315352e3a49154e83bc3e3d442d49417a4875942
Abubakar1010 <abubakar850772@gmail.com>
2024-02-21 06:33:24 +0600

    more about nan

---
+++ exeptions/01_NaN.js
@@ -0,0 +1,12 @@
+let a =[NaN]
+
+// note include method flow same value zero algorithm. that because NaN is equal NaN
+
+// console.log(a.includes(NaN))
+
+// console.log( a === a); //true
+
+let n = NaN
+
+// console.log( n === n); //false
+
