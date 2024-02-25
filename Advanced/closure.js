5f44fef23f4b59fbfca9fab8f0ccc4a48ed0a8de
Abubakar1010 <abubakar850772@gmail.com>
2024-02-25 23:52:59 +0600

    more about closure

--- Advanced/closure.js
+++ Advanced/closure.js
@@ -12,4 +12,24 @@ function kitchen(){
 const fn1 = kitchen()
 
 const fn2 = fn1()
-console.log(fn2);
+// console.log(fn2);
+
+const myFn = ()=> {
+    let count = 0;
+
+    return () => {
+        count++
+
+        return count
+    }
+}
+
+const closed = myFn()
+
+console.log(closed());
+console.log(closed());
+console.log(closed());
+
+//note: closure use for still contained data from parent function and use after when need.and js not killed those data that use in child function
+
+// t-i-q: in js garbage are collected by js. if we call fn js automatically collect unusual data and killed them. but when we use closure concept js not killed those data that related to the closure 
