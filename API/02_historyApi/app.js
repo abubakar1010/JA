a590522ae10dd1d878ecb54481594340c7f7fed0
Abubakar1010 <abubakar850772@gmail.com>
2024-02-26 15:13:08 +0600

    history api

---
+++ API/02_historyApi/app.js
@@ -0,0 +1,12 @@
+ function Forward (){
+    window.history.forward()
+ }
+ function Back (){
+    window.history.back()
+ }
+ function Go (){
+    window.history.go()
+ }
+ function History (){
+    alert(window.history.length)
+ }
